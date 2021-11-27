914e970d23b6a246dcf032a1636b367398d81b0a
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2021-11-27 14:13:56 -0600

    chore: starting over

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,7 @@
 <template>
-  <div>
-    <section>
-      <h1>{{ post.title }}</h1>
-      <p>{{ post.description }}</p>
-      <nuxt-content :document="post"></nuxt-content>
-      <!-- security issue  -->
-      <br />
-      <hr />
-      <br />
-
-      <div v-html="mdBio"></div>
-      <!-- <div>{{ mdBio }}</div> -->
-    </section>
-    <!-- <pre>{{ posts }}</pre> -->
-  </div>
+  <div></div>
 </template>
 
 <script>
-// localhost:1337/bands/11
-export default {
-  // do not have a data end point yet
-}
+export default {}
 </script>
