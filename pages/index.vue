0a5f35b32adaed70e0fb1722b3f7edfe8605ab19
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2021-11-27 14:01:37 -0600

    chore: setup

--- pages/index.vue
+++ pages/index.vue
@@ -19,18 +19,6 @@
 <script>
 // localhost:1337/bands/11
 export default {
-  async asyncData({ $axios, $md, $content }) {
-    // console.log('this is running')
-    const post = await $content('hello').fetch()
-    const bands = await $axios.$get('http://localhost:1337/bands/11')
-    const mdBio = $md.render(bands.bio)
-    // console.log('this is here in the md ', posts)
-    // console.log('is there bands ', bands)
-    return {
-      bands,
-      mdBio,
-      post,
-    }
-  },
+  // do not have a data end point yet
 }
 </script>
