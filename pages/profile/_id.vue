a49c124a7620af31329ae6be60dc79c9b0f4b7fe
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2021-12-22 21:34:53 -0600

    chore: adding band info in the bio

--- pages/profile/_id.vue
+++ pages/profile/_id.vue
@@ -29,7 +29,32 @@
       </section>
     </section>
     <!-- Additional Information -->
-
+    <section class="px-4">
+      <h4>Band Info</h4>
+      <!-- container for the list items -->
+      <div class="flex">
+        <h6 class="flex-1">Members</h6>
+        <div class="flex-1">
+          <span v-for="member in members" :key="member">{{ member }},</span>
+        </div>
+      </div>
+      <!-- genre -->
+      <div class="flex">
+        <h6 class="flex-1">Genre</h6>
+        <div class="flex-1">
+          <p>{{ genre }}</p>
+        </div>
+      </div>
+      <!-- container for the list items -->
+      <div class="flex pt-4">
+        <h6 class="flex-1 mt-0">Links</h6>
+        <div class="flex-1">
+          <nuxt-link to="/" v-for="link in links" :key="link">
+            {{ link.text }},</nuxt-link
+          >
+        </div>
+      </div>
+    </section>
     <!-- Put the album slider here  -->
     <section class="px-4 pt-8">
       <h3 class="font-bold">Albums</h3>
@@ -84,6 +109,29 @@ export default {
   data() {
     return {
       members: ['Jack Richards', 'Peter Michales', 'Rod Dick', 'Nick Rogers'],
+      genre: 'Punk Rock',
+      upcomingEvents: [
+        'At the Dean Oct 1 7:00pm',
+        'The Wicked Nov 10 7:00pm',
+        'At the Dean Oct 1 7:00pm',
+      ],
+      links: [
+        {
+          text: 'thepunks.com',
+          link: 'https:google.com',
+          icon: 'web.svg',
+        },
+        {
+          text: 'facebook.com',
+          link: 'https:google.com',
+          icon: 'facebook.svg',
+        },
+        {
+          text: 'twiiter',
+          link: 'https:google.com',
+          icon: 'twitter.svg',
+        },
+      ],
     }
   },
 }
