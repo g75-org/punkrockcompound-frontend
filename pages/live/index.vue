f74245f2d0ff338458565141d9215d2651efdd06
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2021-12-23 20:23:46 -0600

    feat: adding live

--- pages/live/index.vue
+++ pages/live/index.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
-    <h1>This is the live stream</h1>
+    <h1>Live Streaming</h1>
+    <section>
+      <iframe
+        width="100%"
+        height="400"
+        src="https://www.youtube.com/embed/4P1lPkJois8"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </section>
   </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style scoped>
+.ytp-cued-thumbnail-overlay-image {
+  background-size: contain !important;
+  display: none;
+}
+</style>
